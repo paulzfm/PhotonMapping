4cf8fbe4c40d3dea3bc7c1d74d8da32fb5a1a103
paulzfm <zhufengminpaul@163.com>
2015-06-23 21:18:49 +0800

    add Texture for Sphere

--- util/Texture.cpp
+++ util/Texture.cpp
@@ -4,19 +4,20 @@ RGB Texture::value(const Point& uv)
 {
     double u = uv.x - (int)uv.x;
     double v = uv.y - (int)uv.y; 
-    u *= (_img.width()-3);
-    v *= (_img.height()-3);
+    u *= _img.width() - 3;
+    v *= _img.height() - 3;
 
     int iu = (int)u;
     int iv = (int)v;
 
-    double tu = u-iu;
-    double tv = v-iv;
+    double tu = u - iu;
+    double tv = v - iv;
 
-    RGB color = _img.get(iu,iv)*(1-tu)*(1-tv)+
-      _img.get(iu+1,iv)*tu*(1-tv)+
-      _img.get(iu,iv+1)*(1-tu)*tv+
-      _img.get(iu+1,iv+1)*tu*tv;
+    RGB color = 
+        _img.get(iu, iv) * (1 - tu) * (1 - tv) +
+        _img.get(iu + 1, iv) * tu * (1 - tv) +
+        _img.get(iu, iv + 1) * (1 - tu) * tv +
+        _img.get(iu + 1, iv + 1) * tu * tv;
 
    return color;
 }
